a8ff7cee687618e045b1f13dfb60bff135c6ed6f
Kimberly Meechan <kimberly.meechan@embl.de>
2021-11-17 14:25:56 +0100

    remember last file path in dialogs

--- src/main/java/de/embl/schwab/crosshair/io/IoHelper.java
+++ src/main/java/de/embl/schwab/crosshair/io/IoHelper.java
@@ -2,16 +2,20 @@ package de.embl.schwab.crosshair.io;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 
 public class IoHelper {
 
+    private static String lastDirPath;
+
     public static String chooseOpenFilePath() {
         String filePath = null;
-        JFileChooser chooser = new JFileChooser();
+        JFileChooser chooser = new JFileChooser(lastDirPath);
         chooser.setFileFilter(new FileNameExtensionFilter("json", "json"));
         int returnVal = chooser.showOpenDialog(null);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             filePath = chooser.getSelectedFile().getAbsolutePath();
+            lastDirPath = new File(filePath).getParent();
         }
 
         return filePath;
@@ -19,11 +23,12 @@ public class IoHelper {
 
     public static String chooseSaveFilePath() {
         String filePath = null;
-        JFileChooser chooser = new JFileChooser();
+        JFileChooser chooser = new JFileChooser(lastDirPath);
         chooser.setFileFilter(new FileNameExtensionFilter("json", "json"));
         int returnVal = chooser.showSaveDialog(null);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             filePath = chooser.getSelectedFile().getAbsolutePath() + ".json";
+            lastDirPath = new File(filePath).getParent();
         }
 
         return filePath;
