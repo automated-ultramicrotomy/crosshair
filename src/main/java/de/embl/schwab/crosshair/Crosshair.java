3b161796abe7e154f2a195a8a583c8639154d69b
Kimberly Meechan <kimberly.meechan@gmail.com>
2023-11-27 20:57:51 +0000

    refactor opening crosshair

--- src/main/java/de/embl/schwab/crosshair/Crosshair.java
+++ src/main/java/de/embl/schwab/crosshair/Crosshair.java
@@ -1,13 +1,19 @@
 package de.embl.schwab.crosshair;
 
 import bdv.util.*;
+import bdv.viewer.Source;
 import de.embl.schwab.crosshair.bdv.BdvBehaviours;
 import de.embl.schwab.crosshair.microtome.MicrotomeManager;
 import de.embl.schwab.crosshair.plane.PlaneManager;
 import de.embl.schwab.crosshair.ui.swing.CrosshairFrame;
 import ij3d.Content;
 import ij3d.Image3DUniverse;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.ARGBType;
+import ij.ImagePlus;
 
+import static de.embl.cba.tables.ij3d.UniverseUtils.addSourceToUniverse;
 import static de.embl.schwab.crosshair.utils.Utils.spaceOutWindows;
 
 // TODO - neaten up code structure, possibly clearer labelling of which coordinate system & units are being used
@@ -33,7 +39,59 @@ public class Crosshair {
 	public static final String block = "block";
 	public static final String image = "image";
 
-	public Crosshair (BdvStackSource bdvStackSource, Image3DUniverse universe, Content imageContent, String unit) {
+	// TODO - make generic? Not just 8 bit
+	private final int min = 0;
+	private final int max = 255;
+	private final float transparency = 0.7f;
+
+	/**
+	 * Open Crosshair from a Source (normally from a bdv style file e.g. hdf5 / n5)
+	 * @param imageSource image source
+	 */
+	public Crosshair(Source<Object> imageSource) {
+
+		BdvStackSource bdvStackSource = BdvFunctions.show(imageSource, 1);
+		Image3DUniverse universe = new Image3DUniverse();
+		universe.show();
+
+		String unit = imageSource.getVoxelDimensions().unit();
+
+		// Set to arbitrary colour
+		ARGBType colour =  new ARGBType( ARGBType.rgba( 0, 0, 0, 0 ) );
+		Content imageContent = addSourceToUniverse(universe, imageSource, 300 * 300 * 300,
+				Content.VOLUME, colour, transparency, min, max );
+		// Reset colour to default for 3D viewer
+		imageContent.setColor(null);
+
+		initialiseCrosshair(bdvStackSource, universe, imageContent, unit);
+	}
+
+	/**
+	 * Open Crosshair from an ImagePlus (normally by pulling current image from ImageJ)
+	 * @param imagePlus image
+	 */
+	public Crosshair(ImagePlus imagePlus) {
+
+		Image3DUniverse universe = new Image3DUniverse();
+		Content imageContent = universe.addContent(imagePlus, Content.VOLUME);
+		imageContent.setTransparency(transparency);
+		universe.show();
+
+		final double pw = imagePlus.getCalibration().pixelWidth;
+		final double ph = imagePlus.getCalibration().pixelHeight;
+		final double pd = imagePlus.getCalibration().pixelDepth;
+		final String unit = imagePlus.getCalibration().getUnit();
+
+		final Img wrap = ImageJFunctions.wrap(imagePlus);
+		BdvStackSource bdvStackSource = BdvFunctions.show(wrap, "raw", Bdv.options()
+				.sourceTransform(pw, ph, pd));
+
+		initialiseCrosshair(bdvStackSource, universe, imageContent, unit);
+	}
+
+	private void initialiseCrosshair(BdvStackSource bdvStackSource, Image3DUniverse universe, Content imageContent, String unit) {
+
+		bdvStackSource.setDisplayRange(min, max);
 
 		BdvHandle bdvHandle = bdvStackSource.getBdvHandle();
 		imageContent.setLocked(true);
@@ -54,4 +112,5 @@ public class Crosshair {
 
 		spaceOutWindows( bdvHandle, crosshairFrame, universe );
 	}
+	
 }
