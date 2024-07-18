8b2a3957156bf6374ee68f5bfe33909020902301
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2024-07-18 21:33:02 +0100

    add failure messages

--- src/test/java/de/embl/schwab/crosshair/CrosshairTest.java
+++ src/test/java/de/embl/schwab/crosshair/CrosshairTest.java
@@ -47,20 +47,22 @@ class CrosshairTest {
             Window window = windows[i];
 
             // Check height within screen limits
-            assertTrue(window.getLocationOnScreen().y >= 0);
-            assertTrue(window.getLocationOnScreen().y + window.getHeight() <= screenSize.height);
+            assertTrue(window.getLocationOnScreen().y >= 0,
+                    "window " + i + "'s y min is off screen");
+            assertTrue(window.getLocationOnScreen().y + window.getHeight() <= screenSize.height,
+                    "window " + i + "'s y max is off screen");
 
             xMins[i] = window.getLocationOnScreen().x;
             xMaxes[i] = window.getLocationOnScreen().x + window.getWidth();
 
             // check width within screen limits
-            assertTrue(xMins[i] >= 0);
-            assertTrue(xMaxes[i] <= screenSize.width);
+            assertTrue(xMins[i] >= 0, "window " + i + "'s x min is off screen");
+            assertTrue(xMaxes[i] <= screenSize.width, "window " + i + "'s x max is off screen");
         }
 
         // Check windows don't overlap
-        assertTrue(xMaxes[0] <= xMins[1]);
-        assertTrue(xMaxes[1] <= xMins[2]);
+        assertTrue(xMaxes[0] <= xMins[1], "window 0's right edge overlaps with window 1's left");
+        assertTrue(xMaxes[1] <= xMins[2], "window 1's right edge overlaps with window 2's left");
     }
 
 }
